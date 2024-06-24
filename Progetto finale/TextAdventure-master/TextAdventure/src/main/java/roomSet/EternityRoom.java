a040f18975215fdd678e9303d675077b0dd0cd7e
Gabriele Melucci <gabriele.melucci@gmail.com>
2024-06-24 04:31:25 +0200

    other fix

--- Progetto finale/TextAdventure-master/TextAdventure/src/main/java/roomSet/EternityRoom.java
+++ Progetto finale/TextAdventure-master/TextAdventure/src/main/java/roomSet/EternityRoom.java
@@ -5,8 +5,8 @@ import gameCore.Player;
 
 public class EternityRoom extends RoomWDoor {
 
-    private boolean g = false; // Variabile di istanza per mantenere lo stato tra le chiamate
-    private boolean h = false; // Variabile di istanza per mantenere lo stato tra le chiamate
+    private boolean g = false;
+    private boolean h = false;
 
     public EternityRoom() {
         Stobj obj = new Stobj();
@@ -14,9 +14,10 @@ public class EternityRoom extends RoomWDoor {
         obj.setAlias(new String[] { "buco" });
         obj.setDescription("Una semplice serratura, sarebbe bello avere la chiave corretta per poterla aprire...");
         this.addObject(obj);
-        
+
         Stobj weirdDoor = new Stobj();
         weirdDoor.setName("strana porta");
+        weirdDoor.setAlias(new String[] { "porta" });
         weirdDoor.setDescription("Una porta nera con un incavo a forma di cuore. Non sembra possa essere aperta.");
         weirdDoor.setVisible(false);
         this.addObject(weirdDoor);
@@ -26,7 +27,18 @@ public class EternityRoom extends RoomWDoor {
     public void insert(Player p) {
         int k = -1;
         int l = -1;
-        
+
+        // Cerca il cuore pulsante nell'inventario
+        if (g && !h) { // Solo se g è true e h è false, cerchiamo il cuore pulsante
+            for (int i = 0; i < p.getInventory().size(); i++) {
+                if (p.getInventory().get(i).getName().equals("cuore pulsante")) {
+                    h = true;
+                    l = i;
+                    break;
+                }
+            }
+        }
+
         // Cerca la chiave nell'inventario
         if (!g) { // Solo se g è false, cerchiamo la chiave
             for (int i = 0; i < p.getInventory().size(); i++) {
@@ -37,28 +49,19 @@ public class EternityRoom extends RoomWDoor {
                 }
             }
         }
-        
-        // Cerca il cuore pulsante nell'inventario
-        if (g && !h) { // Solo se g è true e h è false, cerchiamo il cuore pulsante
-            for (int i = 0; i < p.getInventory().size(); i++) {
-                if (p.getInventory().get(i).getName().equals("Cuore pulsante")) {
-                    h = true;
-                    l = i;
-                    break;
-                }
-            }
-        }
-        
+
         // Gestisci l'inserimento degli oggetti in base allo stato di g e h
         if (g && h) {
-            this.setMsg("Inserisci nell'incavo sulla porta il cuore pulsante.\nDopo pochi istanti, il cuore smette di pulsare e la porta si apre.\nOra puoi proseguire verso nord.");
+            this.setMsg(
+                    "Inserisci nell'incavo sulla porta il cuore pulsante.\nDopo pochi istanti, il cuore smette di pulsare e la porta si apre.\nOra puoi proseguire verso nord.");
             if (l != -1) {
                 p.removeFromInventory(l);
             }
             this.getObjects().removeIf(obj -> obj.getName().equals("strana porta"));
             this.setNorth(this.getNextNorth());
-        } else if (g) {
-            this.setMsg("Appena avvicini la chiave nella serratura, si inserisce di colpo, come se attratta da un magnete.\nNon riesci a capire cosa stia succedendo.\nLa parete a nord che sembrava essere così lontana è improvvisamente di fronte a te.\nOra che la parete è così vicina, vedi finalmente la porta.");
+        } else if (g && !h) {
+            this.setMsg(
+                    "Appena avvicini la chiave nella serratura, si inserisce di colpo, come se attratta da un magnete.\nNon riesci a capire cosa stia succedendo.\nLa parete a nord che sembrava essere così lontana è improvvisamente di fronte a te.\nOra che la parete è così vicina, vedi finalmente la porta.");
             if (k != -1) {
                 p.removeFromInventory(k);
             }
