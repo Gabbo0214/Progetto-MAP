4091ae6dc561915f772837b91770333fce9dc07d
Gabriele Melucci <gabriele.melucci@gmail.com>
2024-06-26 22:41:23 +0200

    UI correction

--- Il_Castello_enigmatico/src/main/java/gameInterface/VisibilityManager.java
+++ Il_Castello_enigmatico/src/main/java/gameInterface/VisibilityManager.java
@@ -112,6 +112,7 @@ public class VisibilityManager {
         ui.getYnPanel().setVisible(false);
         ui.getReturnToMenu().setVisible(false);
         ui.getMainTextAreaExit().setVisible(false);
+        ui.getLeaderboardButton().setVisible(false);
     }
 
      /**
@@ -146,6 +147,7 @@ public class VisibilityManager {
         ui.getYnPanel().setVisible(false);
         ui.getReturnToMenu().setVisible(false);
         ui.getMainTextAreaExit().setVisible(false);
+        ui.getLeaderboardButton().setVisible(false);
 
         ui.getMainTextArea().setVisible(true);
         ui.getMainTextPanel().setVisible(true);
@@ -186,6 +188,7 @@ public class VisibilityManager {
          ui.getInventoryPanel().setVisible(false);
          ui.getChoiceButtonPanel().setVisible(false);
          ui.getMainTextArea().setVisible(false);
+         ui.getLeaderboardButton().setVisible(false);
          
          ui.getMainTextAreaExit().setVisible(true);
          ui.getYnPanel().setVisible(true);
@@ -213,6 +216,7 @@ public class VisibilityManager {
          ui.getMainTextArea().setVisible(false);
          ui.getYesButton().setVisible(false);
          ui.getNoButton().setVisible(false);
+         ui.getLeaderboardButton().setVisible(false);
          
          ui.getMainTextAreaExit().setVisible(true);
          ui.getYnPanel().setVisible(true);
